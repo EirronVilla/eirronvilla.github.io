117724119cfada97a79d35443f1142f48c22c201
Aaron Villalobos Arguedas <aaron.villalobos.arguedas@est.una.ac.cr>
2026-09-05 23:43:25 -0600

    Updated generic content

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'EVA';
-export const SITE_DESCRIPTION = 'Ideas para un manana mas sereno.';
+export const SITE_DESCRIPTION = 'Un blog interativo sobre tecnologia y temas diversos.';
