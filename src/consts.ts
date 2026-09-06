9071c9ec574998e247280a2ef5d941ced1859bf6
Aaron Villalobos Arguedas <aaron.villalobos.arguedas@est.una.ac.cr>
2026-09-05 23:29:43 -0600

    Home corrections

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'EVA Project';
+export const SITE_TITLE = 'EVA';
 export const SITE_DESCRIPTION = 'Ideas para un manana mas sereno.';
